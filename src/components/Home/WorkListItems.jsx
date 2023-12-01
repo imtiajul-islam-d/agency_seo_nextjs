97a32eaccaa399c519861da68705d986012e750b
imtiajul-islam-d <imtiajul@hotmail.com>
2023-12-01 09:23:20 +0600

    worklist component modified

--- src/components/Home/WorkListItems.jsx
+++ src/components/Home/WorkListItems.jsx
@@ -11,11 +11,14 @@ const WorkListItems = async () => {
   const data = await getData();
   return (
     <div>
-      <div className="grid grid-cols-1 gap-5 mt-5 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-2 mt-5 md:gap-5 md:grid-cols-2 lg:grid-cols-3">
         {data?.map((item) => (
-          <div className="py-[3.125rem] px-[1.875rem]" key={item?.id}>
+          <div
+            className="py-[3.125rem] group rounded-lg hover:bg-primary-50 transition hover:shadow-sm cursor-pointer px-[1.875rem]"
+            key={item?.id}
+          >
             <div className="max-w-[18.688rem]">
-              <span className="text-[2.063rem] p-[1.375rem] bg-primary-50 inline-block rounded-3xl">
+              <span className="text-[2.063rem] p-[1.375rem] bg-primary-50 transition group-hover:bg-neutral-50 inline-block rounded-3xl">
                 <RiMessengerLine />
               </span>
               <h3 className="mt-2 mb-2 text-2xl font-semibold leading-9 font-font_poppins">
