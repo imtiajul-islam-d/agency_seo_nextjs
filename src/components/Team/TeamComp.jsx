0425313d4168ac7be522f68961b17a8366726424
imtiajul-islam-d <imtiajul@hotmail.com>
2023-12-02 11:45:31 +0600

    team page completed

--- src/components/Team/TeamComp.jsx
+++ src/components/Team/TeamComp.jsx
@@ -1,18 +1,10 @@
 import NewsLetter from "../Shared/NewsLetter";
 import PageHeader from "../Shared/PageHeader";
 import TeamMembers from "./TeamMembers";
-async function getData() {
-  const res = await fetch(process.env.BASE_URL + "api/TeamList");
-  if (!res.ok) {
-    throw new Error("something went wrong");
-  }
-  return res.json();
-}
 
 const TeamComp = async () => {
-  const data = await getData();
   return (
-    <section>
+    <section className="">
       <PageHeader title="Team" breadcrumb="team" />
       <TeamMembers />
       <NewsLetter />
--- src/components/Team/TeamMembers.jsx
+++ src/components/Team/TeamMembers.jsx
@@ -1,8 +1,31 @@
-const TeamMembers = () => {
+import ComponentTitle from "../Shared/ComponentTitle";
+import TeamItem from "./TeamItem";
+
+async function getData() {
+  const res = await fetch(process.env.BASE_URL + "api/TeamList");
+  if (!res.ok) {
+    throw new Error("something went wrong");
+  }
+  return res.json();
+}
+const TeamMembers = async () => {
+  const data = await getData();
   return (
-    <div>
-      <div>Team members comp</div>
-    </div>
+    <section className="my-20">
+      <div className="mx-auto max_w padding">
+        <div className="my-10">
+          <ComponentTitle
+            title="Our Team Member"
+            heading="Check our awesome team members"
+          />
+        </div>
+        <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 md:gap-7">
+          {data?.map((item) => (
+            <TeamItem key={item?.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </section>
   );
 };
 
