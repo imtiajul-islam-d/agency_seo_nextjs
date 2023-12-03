52ff002beab28d9517ed767c579dd18ebdf63ecf
imtiajul-islam-d <imtiajul@hotmail.com>
2023-12-03 07:59:54 +0600

    complete

--- src/components/Shared/SiteNav.jsx
+++ src/components/Shared/SiteNav.jsx
@@ -128,20 +128,24 @@ const SiteNav = () => {
                   onClick={() => setDropdown((e) => !e)}
                   className="p-2 text-gray-600 transition bg-gray-100 rounded hover:text-gray-600/75"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-5 h-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
+                  {dropdown ? (
+                    <p className="w-5 h-5">X</p>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-5 h-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  )}
                 </button>
               </div>
             </div>
